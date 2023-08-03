0931323b99972c6e47586d613bf42c4f4ec54c30
SulthanQintara <sulthanqintara@gmail.com>
2023-08-03 19:11:12 +0700

    Remove unused var and gql codegen

--- src/pages/CollectionList/Detail/index.tsx
+++ src/pages/CollectionList/Detail/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CollectionDetail = () => {
   return (
     <div>CollectionDetail</div>
--- src/pages/CollectionList/index.tsx
+++ src/pages/CollectionList/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CollectionList = () => {
   return (
     <div>CollectionList</div>
